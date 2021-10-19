476805b1160ebc24040279272f4315bc226b0832
Nicolás Valencia Trujillo <nvalenciat@unal.edu.co>
2021-10-19 18:49:16 -0500

    taller 2 Java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,42 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento asientos[];
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	 int cantidadAsientos() {
+		int contador = 0;
+		for (int i = 0 ; i < asientos.length ; i++) {
+			if (asientos[i] instanceof Asiento) {
+				contador++;
+			}
+		
+		}
+		return contador;
+		
+	}
+	 
+	 
+	 String verificarIntegridad() {
+		 
+		 if (motor.registro != registro) {
+			 return "Las piezas no son originales";
+		 }
+		 
+		 for (int i = 0; i < asientos.length; i++) {
+			 if (asientos[i] instanceof Asiento) {
+				 if (asientos[i].registro != registro) {
+					 return "Las piezas no son originales";
+				 }
+			 }
+		 }
+		 return "Auto original";
+	 }
+	 
+	 
+}
